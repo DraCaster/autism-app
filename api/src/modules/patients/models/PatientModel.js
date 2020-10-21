1f3a0d8941c8447eef79fc0da672ede7ff7d7f63
dracaster <lujanrojas.informatica@gmail.com>
2020-10-20 22:00:08 -0300

    update

--- api/src/modules/patients/models/PatientModel.js
+++ api/src/modules/patients/models/PatientModel.js
@@ -1,19 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 //Habilidades de la evaluacion inicial
 const PatientSchema = new mongoose.Schema({
-    name:{type: String},
-    lastname:{type: String},
-    dateOfBirth:{type: Date},
-    treatmentStartDate:{type: Date},
-    coordinator:{type: String},
-    therapist: {type: String},
-    observations: {type: String},
-    evaluations: [{type: mongoose.Schema.Types.ObjectId,
-           ref: "Evaluation"}],
-           
-})
+  name: { type: String },
+  lastname: { type: String },
+  dateOfBirth: { type: Date },
+  treatmentStartDate: { type: Date },
+  coordinator: { type: String },
+  therapist: { type: String },
+  observations: [{ type: mongoose.Schema.Types.ObjectId, ref: "Observation" }],
+  evaluations: [{ type: mongoose.Schema.Types.ObjectId, ref: "Evaluation" }],
+});
 
-const PatientModel = mongoose.model('Patient', PatientSchema);
+const PatientModel = mongoose.model("Patient", PatientSchema);
 
-module.exports = PatientModel
+module.exports = PatientModel;
